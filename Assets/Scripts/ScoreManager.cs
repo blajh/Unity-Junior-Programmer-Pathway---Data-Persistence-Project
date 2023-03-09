d787da7598f50615ab28e06715d0a96c72fbd1bd
Andrej Kovacevic <glazba@gmail.com>
2023-03-09 20:22:08 +0100

    ScoreManager Singleton

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+
+    [SerializeField] private TMP_InputField input;
+
+    private string playerName;
+
+    private void Awake() {
+
+        if (Instance != null && Instance != this) {
+            Destroy(this);
+        } else {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadStartLevel() {
+        playerName = GetPlayerName();
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit() {
+
+        if (Application.isEditor) {
+            EditorApplication.ExitPlaymode();
+        } else {
+            Application.Quit();
+        }
+    }
+
+    private string GetPlayerName() {
+        return input.text;
+    }
+}
